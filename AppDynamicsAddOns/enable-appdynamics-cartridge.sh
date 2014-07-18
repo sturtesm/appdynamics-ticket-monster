154a28d4558a31760445363e5454e80780c62d64
steve.sturtevant <steve.sturtevant@appdynamics.com>
2014-07-18 16:43:14 -0500

    updating config values

--- AppDynamicsAddOns/enable-appdynamics-cartridge.sh
+++ AppDynamicsAddOns/enable-appdynamics-cartridge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rhc add-cartridge -a jbossticketmonster \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_HOST=stevetest.saas.appdynamics.com \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_PORT=443 \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_TIER_NAME=Tickets \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_SSL_ENABLED=true \
+-e APPDYNAMICS_CONTROLLER_HOST_NAME=stevetest.saas.appdynamics.com \
+-e APPDYNAMICS_CONTROLLER_PORT=443 \
+-e APPDYNAMICS_AGENT_TIER_NAME=Tickets \
+-e APPDYNAMICS_CONTROLLER_SSL_ENABLED=true \
 -e APPDYNAMICS_AGENT_ACCOUNT_NAME=stevetest \
 -e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=413540496769 \
 -c https://raw.githubusercontent.com/Appdynamics/openshift-appdynamics-jboss-cartridge/master/metadata/manifest.yml
