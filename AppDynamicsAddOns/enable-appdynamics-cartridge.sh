58956cef9fcebfacd5f81f25be2986f028e0c091
steve.sturtevant <steve.sturtevant@appdynamics.com>
2014-07-18 14:54:04 -0500

    updating perf test, and saas environment

--- AppDynamicsAddOns/enable-appdynamics-cartridge.sh
+++ AppDynamicsAddOns/enable-appdynamics-cartridge.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 rhc add-cartridge -a jbossticketmonster \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_HOST=jbugdemo.saas.appdynamics.com \
+-e OPENSHIFT_APPD_JAVA_CONTROLLER_HOST=stevetest.saas.appdynamics.com \
 -e OPENSHIFT_APPD_JAVA_CONTROLLER_PORT=443 \
 -e OPENSHIFT_APPD_JAVA_CONTROLLER_TIER_NAME=Tickets \
 -e OPENSHIFT_APPD_JAVA_CONTROLLER_SSL_ENABLED=true \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_APP_NAME=OpenShiftTicketMonster \
+-e APPDYNAMICS_AGENT_ACCOUNT_NAME=stevetest \
+-e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=413540496769 \
 -c https://raw.githubusercontent.com/Appdynamics/openshift-appdynamics-jboss-cartridge/master/metadata/manifest.yml
 
