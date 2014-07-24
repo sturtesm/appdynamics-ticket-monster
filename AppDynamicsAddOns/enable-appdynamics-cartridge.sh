71d73c01e5e603f507824d125b8adf67a9f812d7
steve.sturtevant <steve.sturtevant@appdynamics.com>
2014-07-24 14:24:07 -0400

    committing updated start/stop scripts

--- AppDynamicsAddOns/enable-appdynamics-cartridge.sh
+++ AppDynamicsAddOns/enable-appdynamics-cartridge.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 rhc add-cartridge -a jbossticketmonster \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_HOST=stevetest.saas.appdynamics.com \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_PORT=443 \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_TIER_NAME=Tickets \
--e OPENSHIFT_APPD_JAVA_CONTROLLER_SSL_ENABLED=true \
--e APPDYNAMICS_AGENT_ACCOUNT_NAME=stevetest \
--e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=413540496769 \
+-e APPDYNAMICS_CONTROLLER_HOST_NAME=jbugdemo.saas.appdynamics.com \
+-e APPDYNAMICS_AGENT_ACCOUNT_NAME=jbugdemo \
+-e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=6cc1cd9d4d66 \
+-e APPDYNAMICS_CONTROLLER_PORT=443 \
+-e APPDYNAMICS_AGENT_TIER_NAME=Tickets \
+-e APPDYNAMICS_CONTROLLER_SSL_ENABLED=true \
+-e OPENSHIFT_APPD_JAVA_CONTROLLER_APP_NAME=OpenShiftTicketMonster \
 -c https://raw.githubusercontent.com/Appdynamics/openshift-appdynamics-jboss-cartridge/master/metadata/manifest.yml
 
